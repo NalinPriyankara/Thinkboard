48f408ccc0821a329b7461621237a23b978a4e8c
NalinPriyankara <nalinpriyankara033@gmail.com>
2025-07-21 22:11:34 +0530

    frontend begin

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,32 @@ import express from 'express';
 import Routes from './src/routes/Routes.js';
 import connectDB from './src/config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './src/middleware/rateLimiter.js';
+import cors from 'cors';
 
 dotenv.config();    //used to add the mongodb url to .env file due to security issues
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 //middelware
-app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+app.use(express.json());    //parse JSON bodies: req.body
+app.use(rateLimiter);
+
+// app.use((req, res, next) => {
+//     console.log(`Rerq method is ${req.method} & Req URL is ${req.url}`);
+//     next();
+// });
 
 app.use('/api/notes', Routes);
 
-app.listen(5001, () => {
+connectDB().then(() => {
+    app.listen(5001, () => {
     console.log('Server started on PORT:', PORT);
-})
+    });
+});
+
 
